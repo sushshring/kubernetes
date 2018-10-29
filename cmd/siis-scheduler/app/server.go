b9fd8cec1d887c780a0403c52f2130774bd5eb61
Sushrut Shringarputale <contact@sushshring.me>
2018-10-29 11:35:16 -0400

    Added boilerplate v1

---
+++ cmd/siis-scheduler/app/server.go
@@ -0,0 +1,7 @@
+package app
+
+import "github.com/spf13/cobra"
+
+func NewSiisSchedulerCommand() *cobra.Command {
+	return nil
+}
--- cmd/siis-scheduler/siis-scheduler.go
+++ cmd/siis-scheduler/siis-scheduler.go
@@ -1,1 +1,29 @@
 package siis_scheduler
+
+import (
+	. "flag"
+	"fmt"
+	"github.com/spf13/pflag"
+	"github.com/sushshring/kubernetes/cmd/siis-scheduler/app"
+	"github.com/sushshring/kubernetes/pkg/kubectl/util/logs"
+	. "k8s.io/apiserver/pkg/util/flag"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	command := app.NewSiisSchedulerCommand()
+
+	pflag.CommandLine.SetNormalizeFunc(WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(CommandLine)
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
